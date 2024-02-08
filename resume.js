6a5a1390cf3517a4d19cd377178cd78e80450caf
arre_ankit <arre.ankit@gmail.com>
2024-02-08 15:33:20 +0530

    cli resume tool

---
+++ resume.js
@@ -0,0 +1,51 @@
+module.exports = {
+    name: "Ankit Kumar",
+    address: "Varansi, India",
+    birthday: "26/12/2004",
+    contacts:{
+        email: "arre.ankit@gmail.com",
+        "phone": "+91 9928277157",
+    },
+    links:{
+        Github: "github.com/arre-ankit",
+        Linkedin: "linkedin.com/in/arre-ankit",
+        Twitter: "twitter.com/arre_ankit",
+    },
+    education: 
+        [{
+            from: "2021",
+            to: "2025",
+            institution: "IIT BHU",
+            degree: "B.Tech Civil Engineering",
+        },
+        {   
+            from: "2019",
+            to: "2021",
+            institution: "Decent Public School",
+            degree: "12th",
+        }],
+    skills: {
+        programming: ["JavaScript", "TypeScript", "Python", "C++"],
+        web: ["React", "Node", "Express", "MongoDB"],
+    },
+    projects: [
+        {
+            name: "Project 1",
+            description: "Description 1",
+            link: "Link 1",
+        },
+        {
+            name: "Project 2",
+            description: "Description 2",
+            link: "Link 2",
+        },
+        {
+            name: "Project 3",
+            description: "Description 3",
+            link: "Link 3",
+        }
+    ]
+    }
+
+    
+
